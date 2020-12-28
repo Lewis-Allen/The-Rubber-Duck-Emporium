19a8824aa013a184b2f8418833f896a5c54d32f3
Lewis-Allen <lewisn11@hotmail.co.uk>
2020-12-28 00:06:26 +0000

    Add some buttons on basket page.

--- RubberDuckEmporium/Server/Controllers/BasketController.cs
+++ RubberDuckEmporium/Server/Controllers/BasketController.cs
@@ -14,6 +14,7 @@ namespace RubberDuckEmporium.Server.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
+    [Authorize(AuthenticationSchemes="Bearer")]
     public class BasketController : ControllerBase
     {
         private readonly AppDbContext _context;
